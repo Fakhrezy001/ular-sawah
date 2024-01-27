d97a36b146ba3d94b981809902f3c755281fd6ce
Fakhrezy <dedenfahrulroziqin@gmail.com>
2024-01-27 15:50:19 +0700

    nambah __pycache__

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+# halo dek
 print("halo dek")
-
+# ini output belajar
 print("ayo belajar")
+
+# ! ini comment alert
+print("Syntax error")
+
+# ? ini comment question
+print("apa itu?")
+
+# todo  ini comment list
+print("makan")
+
+# // ini comment coret
+print("maling")
+
+# * ini comment betul
+print("solat")
+
+"""
+    ini comment
+    makan tidur ngoding
+    wkwkwk mampus
+"""
+"""_compile python ke bytcode_
+    pada terminal tulis : python -m py_compile (nama file).py
+"""
